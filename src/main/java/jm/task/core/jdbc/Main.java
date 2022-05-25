d77c335efe725df5a26e241bdcc3c605bd0487c1
MinibaevaElvira <minibaevaelvirar@gmail.com>
2022-05-25 21:58:47 +0300

    PP 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Антон", "Чехов", (byte) 44);
+        userDao.saveUser("Альбер", "Камю", (byte) 46);
+        userDao.saveUser("Франц", "Кафка", (byte) 41);
+        userDao.saveUser("Уильям", "Моэм", (byte) 91);
+
+        userDao.getAllUsers();
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+     public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            String userNAme = "root";
+            String password = "Password";
+            String url = "jdbc:MySQL://localhost/schema?useSSL=false&allowPublicKeyRetrieval=true";
+            connection = DriverManager.getConnection(url, userNAme, password);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    return connection;
+    }
 }
